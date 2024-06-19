e7643bc835bc4149264e1f69f0fca19be8b1c436
Hiepduong040 <147359314+Hiepduong040@users.noreply.github.com>
2024-06-19 14:48:23 +0700

    ConfirmModal.tsx

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
 
-export default function ConfirmModal() {
-    return (
-        <div className="overlay" hidden>
-          <div className="modal-custom">
-            <div className="modal-title">
-              <h4>Cảnh báo</h4>
-              <i className="fa-solid fa-xmark" />
-            </div>
-            <div className="modal-body-custom">
-              <span>Bạn có chắc chắn muốn chặn tài khoản này?</span>
-            </div>
-            <div className="modal-footer-custom">
-              <button className="btn btn-light">Hủy</button>
-              <button className="btn btn-danger">Xác nhận</button>
-            </div>
-          </div>
+type ConfirmModalProps = {
+  onClose: () => void;
+  onConfirm: () => void;
+};
+
+export default function ConfirmModal({ onClose, onConfirm }: ConfirmModalProps) {
+  return (
+    <div className="overlay">
+      <div className="modal-custom">
+        <div className="modal-title">
+          <h4>Cảnh báo</h4>
+          <i className="fa-solid fa-xmark" onClick={onClose} />
         </div>
-      );
+        <div className="modal-body-custom">
+          <span>Bạn có chắc chắn muốn xóa bản ghi này?</span>
+        </div>
+        <div className="modal-footer-custom">
+          <button className="btn btn-light" onClick={onClose}>Hủy</button>
+          <button className="btn btn-danger" onClick={onConfirm}>Xác nhận</button>
+        </div>
+      </div>
+    </div>
+  );
 }
